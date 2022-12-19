72e2ac89de350d2a96ffb1fceebf16eb258b1c75
youkan <youkan@localhost>
2022-12-19 13:27:50 +0900

    こんにちは

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,37 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserSevice;
+
+/**
+ * ユーザー情報 Controller
+ */
+@Controller
 public class UserController {
 
-	
+  /**
+   * ユーザー情報 Service
+   */
+  @Autowired
+  UserSevice userSevice;
+
+  /**
+   * ユーザー情報一覧画面を表示
+   * @param model Model
+   * @return ユーザー情報一覧画面のHTML
+   */
+  @RequestMapping(value = "/user/list", method = RequestMethod.GET)
+  public String displayList(Model model) {
+    List<User> userlist = userSevice.searchAll();
+    model.addAttribute("userlist", userlist);
+    return "user/list";
+  }
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,5 +1,13 @@
 package com.example.demo.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.example.demo.entity.User;
+
+/**
+ * ユーザー情報 Repository
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
